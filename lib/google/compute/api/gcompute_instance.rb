af9fef71b1787d322d3f6110a9b773fac1800bf4
Alex Stephen <alexstephen@google.com>
2017-10-10 01:00:16 +0000

    removing google/authorization from bolt functions
    Change-Id: I82eb3a020155fba898fc1e06fa557918f97c94dc

--- lib/google/compute/api/gcompute_instance.rb
+++ lib/google/compute/api/gcompute_instance.rb
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'google/authorization'
 require 'google/compute/network/post'
 
 module Google
